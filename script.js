14ff9621dd41d31973894152c601d1f031016a15
Ineza <inezaherve8@gmail.com>
2024-03-25 10:11:35 +0530

    removed my code for offcanvas and added bootsrap offcanvas code

--- script.js
+++ script.js
@@ -4,21 +4,21 @@ const sideBarContainer = document.querySelector("#side-bar");
 const menuBtn = document.querySelector(".bars-btn");
 const closeBtn = document.querySelector(".close-btn");
 
-menuBtn.addEventListener("click", (e) => {
-  e.preventDefault();
+// menuBtn.addEventListener("click", (e) => {
+//   e.preventDefault();
 
-  sideBarContainer.classList.remove("d-none");
-  menuBtn.classList.add("d-none");
+//   sideBarContainer.classList.remove("d-none");
+//   menuBtn.classList.add("d-none");
 
-  closeBtn.classList.remove("d-none");
-  closeBtn.classList.add("d-lx-none");
-});
+//   closeBtn.classList.remove("d-none");
+//   closeBtn.classList.add("d-lx-none");
+// });
 
-closeBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  closeBtn.classList.remove("d-lx-none");
-  closeBtn.classList.add("d-none");
+// closeBtn.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   closeBtn.classList.remove("d-lx-none");
+//   closeBtn.classList.add("d-none");
 
-  menuBtn.classList.remove("d-none");
-  sideBarContainer.classList.add("d-none");
-});
+//   menuBtn.classList.remove("d-none");
+//   sideBarContainer.classList.add("d-none");
+// });
